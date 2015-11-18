632b2bd036cb58813dbf4bcc44003c329b3ced8a
HectorCloud <hectorcloud@gmail.com>
2015-11-18 11:15:54 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Baidu Yun utility, upload to and download from Baidu PCS
+which is located at http://pan.baidu.com.
+
+thanks to bypy and api.py(https://github.com/ly0/baidupcsapi).
+
+under python 3.4.3
+
+references:
+  http://developer.baidu.com/wiki/index.php?title=docs/pcs/guide/usage_example
+  http://developer.baidu.com/wiki/index.php?title=docs/pcs/rest/file_data_apis_list
+Author: Hector Cloud
+Date: Aug 1, 2015
+"""
+
+import sys
+import os
+import re
+import threading
+import json
+import multiprocessing
+import time
+import datetime
+from pcsminimal import *
+import requests.packages.urllib3
+
+def file2download(clouddrive, abspathRemote):
+    """
+    :param clouddrive: PCSMinimal
+    :param path: remote path, already absolute path
+    :return:
+    """
+    requests.packages.urllib3.disable_warnings()
+
+    def helper_file2download(abspathRemote):
+        _files = []
+        if clouddrive.directory_existence(abspathRemote):
+            # create local dir
+            relpath = os.path.relpath(abspathRemote, clouddrive.rootDirRemote)
+            abspath = os.path.join(clouddrive.rootDirLocal, relpath)
+            if not os.path.exists(abspath):
+                os.mkdir(abspath)
+            paths = clouddrive.directory_list2(abspathRemote)
+            for (path, isdir) in paths:
+                if isdir:
+                    filesinsubdir = helper_file2download(path)
+                    _files += filesinsubdir
+                else:
+                    print("info: collect file [{}]".format(path))
+                    _files.append(path)
+
+        if clouddrive.file_existence(abspathRemote):
+            print("info: collect file [{}]".format(abspathRemote))
+            _files.append(abspathRemote)
+
+        return _files
+
+    _files = helper_file2download(abspathRemote)
+    with open("pcstest_oauth", "w") as fd:
+        _files = json.dumps(_files)
+        fd.write(_files)
+    return 0
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    # when does this script start?
+    time_started = datetime.datetime.now()
+
+    requests.packages.urllib3.disable_warnings()
+
+    usage = """
+    {0} upload => from [local dir] to [remote dir]
+    {0} download => from [remote dir] to [local dir]
+    {0} list => show subdirs and files
+    {0} delete => delete remote dir|file
+    Note: remote dir is relative to /apps/pcstest_oauth
+    """.format(os.path.basename(sys.argv[0]))
+
+    if len(sys.argv) < 2:
+        print(usage)
+        exit(0)
+
+    oauth_url = "https://openapi.baidu.com/oauth/2.0/authorize?response_type=token&client_id=L6g70tBRRIXLsY0Z3HwKqlRE&redirect_uri=oob&force_login=1&scope=basic+netdisk"
+    print(oauth_url)
+    access_token = input("please type above url in web browser and input access_token: ")
+    clouddrive = PCSMinimal(access_token)
+
+    operation = sys.argv[1]
+
+    if operation == "upload":
+        dirLocal = input("local dir: ")
+        dirRemote = input("remote dir: ")
+        # reset working directory even though named as 'rootDirXxx'
+        rootDirRemote = os.path.join(clouddrive.rootDirRemote, dirRemote)
+        # normalize path
+        rootDirRemote = os.path.normpath(rootDirRemote)
+        rootDirRemote = rootDirRemote.replace("\\", "/")
+        clouddrive.rootDirRemote = rootDirRemote
+        if not clouddrive.directory_existence(rootDirRemote):
+            clouddrive.directory_creation(rootDirRemote)
+        rootDirLocal = os.path.abspath(dirLocal)
+        clouddrive.rootDirLocal = rootDirLocal
+        # list all files in directory and create directory in Baidu Yun
+        file2upload = []
+        for root, dirs, files in os.walk(rootDirLocal):
+            for dir in dirs:
+                # exclude hidden dirs
+                fullpath = os.path.join(root, dir)
+                if "/." in fullpath:
+                    continue
+                # if not exists in Baidu Yun, create this directory
+                absdir = os.path.join(root, dir)
+                reldir = os.path.relpath(absdir, rootDirLocal)
+                if not clouddrive.directory_existence(reldir):
+                    clouddrive.directory_creation(reldir)
+            for file in files:
+                # exclude hidden dirs
+                if "/." in root:
+                    continue
+                # exclude hidden files
+                if file.startswith("."):
+                    continue
+                # exclude undeleted chunk file last time
+                if re.search(r"\.\d{4}$", file):
+                    continue
+                file = os.path.join(root, file)
+                file2upload.append(file)
+
+        def upload(file2upload, mutex):
+            while True:
+                file = None
+                mutex.acquire()
+                if file2upload:
+                    file = file2upload[0]
+                    file2upload[:] = file2upload[1:]
+                else:
+                    file = None
+                mutex.release()
+                if file:
+                    # upload this file
+                    # chunk size is 1M
+                    size = os.path.getsize(file)
+                    counter = (size+1*1024*1024-1)//(1*1024*1024)
+                    for i in range(counter):
+                        with open(file, "rb") as fdlocal:
+                            fdlocal.seek(i*1*1024*1024, 0)
+                            data = fdlocal.read(1*1024*1024)
+                            # chunk filename, append four digits, like good.mp4.0001
+                            # large as 10G
+                            suffix = "." + ("0"*(4-len(str(i)))) + str(i)
+                            fnchunk = file+suffix
+                            # create this chunk file
+                            with open(fnchunk, "wb") as fdremote:
+                                fdremote.write(data)
+                            # upload this chunk file
+                            clouddrive.file_upload(fnchunk)
+                            # delete this chunk file
+                            os.remove(fnchunk)
+                else:
+                    # no file left to upload
+                    return
+        # upload by thread pool, size is 5*cpu_cores at the moment
+        threadpool = []
+        mutex = threading.Lock()
+        cpu_cores = multiprocessing.cpu_count()
+        # at least 6 workers
+        for i in range(max(5*cpu_cores, 6)):
+            th = threading.Thread(target=upload, args=(file2upload, mutex))
+            th.start()
+            threadpool.append(th)
+        # wait files are uploaded
+        for th in threadpool:
+            th.join()
+
+    if operation == "download":
+        dirRemote = input("remote dir: ")
+        dirLocal = input("local dir: ")
+
+        abspathRemote = os.path.join(clouddrive.rootDirRemote, dirRemote)
+        # normalize remote path
+        abspathRemote = os.path.normpath(abspathRemote)
+        abspathRemote = abspathRemote.replace("\\", "/")
+        dirRemote = abspathRemote
+        clouddrive.rootDirRemote = abspathRemote
+
+        rootDirLocal = os.path.abspath(dirLocal)
+        clouddrive.rootDirLocal = rootDirLocal
+
+        # mkdir if not exists
+        if not os.path.exists(rootDirLocal):
+            os.mkdir(rootDirLocal)
+
+        # thread to download
+        def download(files, mutex):
+            """
+            :param path: remote file path, already absolute path
+            :return:
+            """
+            while True:
+                _abspathRemote = None
+                mutex.acquire()
+                if files:
+                    _abspathRemote = files[0]
+                    files[:] = files[1:]
+                else:
+                    _abspathRemote = None
+                mutex.release()
+                if _abspathRemote:
+                    clouddrive.file_download(_abspathRemote)
+                else:
+                    return
+
+        print("info: collecting files to download")
+        # sockets may be blocked infinitely.
+        # workaround is TIMEOUT
+        files = []
+        while True:
+            p = multiprocessing.Process(target=file2download, args=(clouddrive, abspathRemote))
+            p.start()
+            # estimate is 3 minutes
+            p.join(timeout=3*60)
+            if p.is_alive():
+                p.terminate()
+                time.sleep(2)
+                continue
+            else:
+                # IPC by file
+                with open("pcstest_oauth", "r") as fd:
+                    _files = fd.read()
+                    files = json.loads(_files)
+                os.remove("pcstest_oauth")
+                break
+        # download sequentially
+        files.sort()
+        # make a backup for later use, for example, integrity check
+        #files_backup = list(files)
+
+        # download by thread pool, size is 5*cpu_core at the moment
+        threadpool = []
+        mutex = threading.Lock()
+        cpu_cores = multiprocessing.cpu_count()
+        # at least 6 workers
+        for i in range(max(5*cpu_cores, 6)):
+            th = threading.Thread(target=download, args=(files, mutex))
+            th.start()
+            threadpool.append(th)
+        # wait download completed
+        for th in threadpool:
+            th.join()
+        print("info: download finished")
+
+        # list all files in directory
+        file2merge = []
+        for root, dirs, files in os.walk(rootDirLocal):
+            for file in files:
+                file = os.path.join(root, file)
+                file2merge.append(file)
+        file2merge.sort()
+        # recover file name by strip '.0001' ending
+        filenames = [fn[:-5] for fn in file2merge]
+        filenames = set(filenames)
+        filenames = list(filenames)
+        filenames.sort()
+
+        # integrity check. Are all files downloaded? Are their size is 1M except last one?
+        for fn in filenames:
+            print("info: check file {}".format(fn))
+            chunks = []
+            for chunk in file2merge:
+                if (fn == chunk[:-5]) and re.fullmatch(r"\.\d{4}", chunk[-5:]):
+                    chunks.append(chunk)
+            # each chunk is 1M except last one for each file
+            # cardinality is continuous
+            chunks.sort()
+            for idx in range(len(chunks)-1):
+                chunk = chunks[idx]
+                if int(chunk[-4:]) != idx:
+                    suffix = "." + ("0"*(4-len(str(idx)))) + str(idx)
+                    print("error: {} not exists".format(chunk[:-5] + suffix))
+                    exit(1)
+                if os.path.getsize(chunk) != 1*1024*1024:
+                    print("error: size of {} not equal 1M".format(chunk))
+                    exit(1)
+            # last chunk
+            idx = len(chunks) - 1
+            chunk = chunks[idx]
+            if int(chunk[-4:]) != idx:
+                suffix = "." + ("0"*(4-len(str(idx)))) + str(idx)
+                print("error: {} not exists".format(chunk[:-5] + suffix))
+                exit(1)
+
+        # merge chunk files
+        for fn in filenames:
+            print("info: merge file {}".format(fn))
+            for chunk in file2merge:
+                if (fn == chunk[:-5]) and re.fullmatch(r"\.\d{4}", chunk[-5:]):
+                    with open(chunk, "rb") as fd:
+                        data = fd.read()
+                        fd.close()
+                        with open(fn, "ab") as fd:
+                            fd.write(data)
+                            fd.close()
+                    # remove chunk file due to merged
+                    os.remove(chunk)
+
+    if operation == "list":
+        dirRemote = input("remote dir: ")
+        # absolute path
+        dirRemote = os.path.join(clouddrive.rootDirRemote, dirRemote)
+        dirRemote = os.path.normpath(dirRemote)
+        dirRemote = dirRemote.replace("\\", "/")
+        paths = clouddrive.directory_list(dirRemote)
+        for path in paths:
+            print(path)
+
+    if operation == "delete":
+        dirRemote = input("remote dir: ")
+        # absolute path
+        dirRemote = os.path.join(clouddrive.rootDirRemote, dirRemote)
+        dirRemote = os.path.normpath(dirRemote)
+        dirRemote = dirRemote.replace("\\", "/")
+        # special to "/apps/pcstest_oauth"
+        if dirRemote == "/apps/pcstest_oauth":
+            paths = clouddrive.directory_list(dirRemote)
+            for path in paths:
+                clouddrive.directory_deletion(path)
+        else:
+            clouddrive.directory_deletion(dirRemote)
+
+    # when does this script end?
+    time_finished = datetime.datetime.now()
+
+    # running statistics
+    print("\n")
+    print("time  started: {}".format(time_started))
+    print("time finished: {}".format(time_finished))
+    # file size transferred
+    transferred = 0
+    if operation in ["upload", "download"]:
+        for root, dirs, files in os.walk(clouddrive.rootDirLocal):
+            for file in files:
+                file = os.path.join(root, file)
+                transferred += os.path.getsize(file)
+    # transfer speed
+    time_spend = time_finished - time_started
+    speed = (transferred/1024)/time_spend.total_seconds()
+    if operation in ["upload"]:
+        print("upload size: {} Mb".format(str(round(transferred/1024/1024))))
+        print("time spent: {}".format(time_spend))
+        print("upload speed: {} kbps".format(str(speed)))
+    if operation in ["download"]:
+        print("download size: {} Mb".format(str(round(transferred/1024/1024))))
+        print("time spent: {}".format(time_spend))
+        print("download speed: {} kbps".format(str(speed)))
---
+++ pcsminimal.py
@@ -0,0 +1,286 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Baidu Yun utility, upload to and download from Baidu PCS
+which is located at http://pan.baidu.com.
+
+thanks to bypy and api.py.
+
+under python 3.4.3
+
+Author: Hector Cloud
+Date: Aug 1, 2015
+"""
+
+import os
+import multiprocessing
+import time
+from pcs import *
+
+class PCSMinimal(PCS):
+    # actually is working directory
+    # variable name 'workdir' more appropriate
+    rootDirRemote = "/apps/pcstest_oauth/"
+    # depends on user input
+    rootDirLocal = "."
+
+    def __init__(self, access_token):
+        PCS.__init__(self, access_token)
+
+    def directory_creation(self, dir):
+        # must be absolute path
+        dir = os.path.join(self.rootDirRemote, dir)
+        dir = dir.replace("\\", "/")
+        response = self.mkdir(dir)
+        # check result
+        if not response.ok:
+            print("cannot create directory: {}".format(dir))
+
+    def directory_deletion(self, dir):
+        # must be absolute path
+        dir = os.path.join(self.rootDirRemote, dir)
+        response = self.delete(dir)
+        # check result
+        if not response.ok:
+            print("cannot delete directory: {}".format(dir))
+
+    def directory_existence(self, dir):
+        # must be absolute path
+        dir = os.path.join(self.rootDirRemote, dir)
+        response = self.meta(dir)
+        if not response.ok:
+            return False
+        content = response.json()
+        if content["list"][0]["isdir"]:
+            return True
+        else:
+            return False
+
+    def directory_list(self, dir):
+        """
+        :param dir:
+        :return: subdirectories and files, like['rachel', 'happy.mp3', ...]
+        Note: NOT distinguish directory and file at the moment
+        """
+        # must be absolute path
+        dir = os.path.join(self.rootDirRemote, dir)
+        response = self.list_files(dir, by="name", order="asc")
+        result = []
+        if not response.ok:
+            print("cannot list direcotry: {}".format(dir))
+            return result
+        content = response.json()
+        for it in content["list"]:
+            path = it["path"]
+            result.append(path)
+        result.sort()
+        return result
+
+    def directory_list2(self, dir):
+        """
+        :param dir:
+        :return: subdirectories and files, like[(rachel, 1), '(happy.mp3, 0), ...]
+        Note: NOT distinguish directory and file at the moment
+        """
+        # must be absolute path
+        dir = os.path.join(self.rootDirRemote, dir)
+        response = self.list_files(dir, by="name", order="asc")
+        result = []
+        if not response.ok:
+            print("cannot list direcotry: {}".format(dir))
+            return result
+        content = response.json()
+        for it in content["list"]:
+            path = it["path"]
+            isdir = it["isdir"]
+            result.append((path, isdir))
+        #result.sort()
+        return result
+
+    def file_upload(self, fn):
+        # I don't know the exact reason why requests are blocked whole night.
+        # the workaround is restart uploading after some TIMEOUT.
+        # prefer multiprocessing to threading because there is no Thread.terminate()
+        size = os.path.getsize(fn)
+        # average speed is 5 kbps
+        # residual 5 seconds if empty file
+        timeout = size / (5*1024) + 5
+        while True:
+            p = multiprocessing.Process(target=helper_file_upload, args=(self, fn))
+            p.start()
+            p.join(timeout=timeout)
+            if p.is_alive():
+                # upload not finished yet
+                p.terminate()
+                time.sleep(2)
+                print("info: next round [{}]".format(fn))
+                # next run
+                continue
+            else:
+                # upload finished, integrity check
+                # must be absolute path
+                _fn = os.path.relpath(fn, self.rootDirLocal)
+                _fn = os.path.join(self.rootDirRemote, _fn)
+                _fn = os.path.normpath(_fn)
+                _fn = _fn.replace("\\", "/")
+                response = self.meta(_fn)
+                if response.ok:
+                    # file size check
+                    content = response.json()
+                    if not content["list"][0]["isdir"]:
+                        if content["list"][0]["size"] == size:
+                            # upload success
+                            break
+                print("info: next round [{}]".format(fn))
+                continue
+
+    def file_download(self, fn):
+        # download this file until success
+        while True:
+            p = multiprocessing.Process(target=helper_file_download, args=(self, fn))
+            p.start()
+            # restart if not finished within 5 minutes
+            p.join(timeout=5*60)
+            if p.is_alive():
+                p.terminate()
+                time.sleep(2)
+                print("info: next round [{}]".format(fn))
+                # next round
+                continue
+            else:
+                break
+
+    def file_existence(self, fn):
+        # must be absolute path
+        fn = os.path.join(self.rootDirRemote,fn)
+        response = self.meta(fn)
+        if not response.ok:
+            return False
+        content = response.json()
+        if not content["list"][0]["isdir"]:
+            return True
+        else:
+            return False
+
+    def file_deletion(self, fn):
+        # must be absolute path
+        fn = os.path.join(self.rootDirRemote, fn)
+        response = self.delete(fn)
+        # check result
+        if not response.ok:
+            print("cannot delete directory: {}".format(dir))
+
+
+def helper_file_upload(self, fn):
+    """
+    upload this file until success
+    :param self: PCSMinimal
+    :param fn:
+    :return:
+    """
+    requests.packages.urllib3.disable_warnings()
+    # relative path at local then join as remote path
+    abslocal = os.path.join(self.rootDirLocal, fn)
+    fn = os.path.relpath(fn, self.rootDirLocal)
+    fn = os.path.join(self.rootDirRemote, fn)
+    fn = fn.replace("\\", "/")
+
+    # already uploaded?
+    response = None
+    try:
+        response = self.meta(fn)
+    except Exception as e:
+        print("info: upload exception [{}]".format(str(e)))
+    if hasattr(response, "ok") and response.ok:
+        content = response.json()
+        if content["list"][0]["size"] == os.path.getsize(abslocal):
+            print("info: already uploaded, skip. [{}]".format(fn))
+            return
+
+    print("info: upload start[{}]".format(fn))
+    response = None
+    try:
+        response = self.upload(fn, open(abslocal, "rb"), ondup="overwrite")
+    except Exception as e:
+        print("info: upload exception [{}]".format(str(e)))
+    if (not hasattr(response, "ok")) or (not response.ok):
+        print("error: upload [{}]".format(fn))
+        # try again until success
+        print("info: upload again [{}]".format(fn))
+        helper_file_upload(self, abslocal)
+    else:
+        content = response.json()
+        # size or md5
+        size = content["size"]
+        print("info: upload finish [{}]".format(fn))
+
+
+def helper_file_download(self, fn):
+    """
+    1M at a time
+    Range: bytes=0-99 <= 100 bytes
+    :param self: PCSMinimal
+    :param fn:
+    :return:
+    """
+    requests.packages.urllib3.disable_warnings()
+    # absolute path at remote
+    fn = os.path.join(self.rootDirRemote, fn)
+    fn = fn.replace("\\", "/")
+    print("info: download start [{}]".format(fn))
+
+    # file size
+    response = None
+    try:
+        response = self.meta(fn)
+    except Exception as e:
+        print("info: download exception [{}]".format(str(e)))
+
+    if (not hasattr(response, "ok")) or (not response.ok):
+        helper_file_download(self, fn)
+        return
+
+    content = response.json()
+    size = content["list"][0]["size"]
+
+    relremote = os.path.relpath(fn, self.rootDirRemote)
+    fnlocal = os.path.join(self.rootDirLocal, relremote)
+
+    # already downloaded
+    if os.path.exists(fnlocal):
+        if size == os.path.getsize(fnlocal):
+            print("info: already downloaded, skip. [{}]".format(fn))
+            return
+
+    response = None
+    try:
+        # Range: start-end
+        start = 0
+        if os.path.exists(fnlocal):
+            start = os.path.getsize(fnlocal)
+        else:
+            start = 0
+        end = size - 1
+        if size - start <= 1*1024*1024:
+            end = size - 1
+        else:
+            end = start+(1*1024*1024)-1
+        headers = {'Range': 'bytes={}-{}'.format(str(start), str(end))}
+        response = self.download(fn, headers=headers)
+    except Exception as e:
+        print("info: download exception [{}]".format(str(e)))
+    if (not hasattr(response, "ok")) or (not response.ok):
+        print("error: download not ok.Rang:{}-{}".format(str(start), str(end)))
+        # continue downloading
+        helper_file_download(self, fn)
+    else:
+        content = response._content
+        mode = "ab" if os.path.exists(fnlocal) else "wb"
+        with open(fnlocal, mode) as fd:
+            fd.write(content)
+        if size == os.path.getsize(fnlocal):
+            print("info: download finish [{}]".format(fn))
+        else:
+            print("info: download more data")
+            # continue downloading
+            helper_file_download(self, fn)
